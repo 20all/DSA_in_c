53af5bfee3ea7e1235807778c8c1f10ba1d0a530
20al <bishallimbu00010@gmail.com>
2025-07-24 23:40:25 +0545

    finished the circular queue using structure (without secrificing any memeory space version)

--- Queue/Circular_Queue/circular_queue_using_structure.c
+++ Queue/Circular_Queue/circular_queue_using_structure.c
@@ -8,75 +8,104 @@ typedef struct cQueue {
     int rear;
 } CQueue;
 
-void initialize(CQueue cq) {
-    cq.front = cq.rear = MAX_SIZE -1;
+void initialize(CQueue *cq) {
+    cq->front = cq->rear = -1;
 }
 
-bool isEmpty(CQueue cq) {
-    return cq.rear == cq.front;
+bool isEmpty(CQueue *cq) {
+    return cq->front == -1;
 }
 
-bool isFull(CQueue cq) {
-    return cq.front == (cq.rear +1) % MAX_SIZE;
+bool isFull(CQueue *cq) {
+    return cq->front == (cq->rear +1) % MAX_SIZE;
 }
 
-void enqueue(CQueue cq, int data) {
+void enqueue(CQueue *cq, int data) {
     if(isFull(cq)) {
         printf("Couldn't enqueue '%d' (Circular_Queue Overflow)\n",data);
         return;
     } else {
-        cq.rear = (cq.rear +1) % MAX_SIZE;
-        cq.elements[cq.rear] = data;
+        if(isEmpty(cq)) { 
+            cq->front = cq->rear = 0; // set front and rear to 0 if cQueue is empty(when front and rear == -1)
+        } else {
+            cq->rear = (cq->rear+1)%MAX_SIZE;
+        }
+        cq->elements[cq->rear] = data;
         printf("'%d' enqueued \n",data);
     }
 }
 
-void dequeue(CQueue cq) {
+void dequeue(CQueue *cq) {
     if(isEmpty(cq)) {
         printf("Couldn't dequeue (Circular_Queue Underflow)\n");
         return;
     } else {
-        cq.front = (cq.front+1)%MAX_SIZE;
-        int dequeuedItem = cq.elements[cq.front];
-        printf("'%d' dequeued\n",dequeuedItem);
+        int dequeuedItem = cq->elements[cq->front];
+        if(cq->front == cq->rear) {
+            initialize(cq);
+            // cq->front = cq->rear = -1;
+        } else {
+            cq->front = (cq->front+1)%MAX_SIZE;
+        }
+        printf("Dequeued '%d'\n",dequeuedItem);
     }
 }
 
-void display(CQueue cq) {
+void display(CQueue *cq) {
     if(isEmpty(cq)) {
         printf("Couldn't display (Circular_Queue is empty)\n");
         return;
     } else {
-        for(int i = (cq.front+1)%MAX_SIZE; i != cq.rear; i = (i+1)%MAX_SIZE) {
-            printf(" |%d| ",cq.elements[i]);
+        printf("Circular-Queue elements:\n");
+        printf(">--[ ");
+        // for(int i = cq->front; i <= cq->rear; i++) {
+        //     printf(" %d_ ",cq->elements[i]);
+        // }
+        int counter = 0;
+        int i = cq->front;
+        while (1) {
+            printf("  %d_ ",cq->elements[i]);
+            counter++;
+            if(i == cq->rear) break;
+            i = (i +1) % MAX_SIZE;
         }
-        printf("Front: %d and Rear: %d\n",cq.front, cq.rear);
+        printf(" ]-->");
+        printf("\nFront: %d and Rear: %d\n",cq->front, cq->rear);
+        // int availableSpace = (cq->front)%MAX_SIZE + (MAX_SIZE - (cq->rear)%MAX_SIZE) -1 ;// (front+1)%MAX_SIZE + (MAX_SIZE - (rear+1)%MAX_SIZE)
+        int availableSpace = MAX_SIZE - counter;
+        printf("Available space: %d units\n\n",availableSpace);
     }
 }
 
 int main() {
     CQueue q;
-    initialize(q);
-    display(q);
-    enqueue(q, 1);
-    enqueue(q, 2);
-    enqueue(q, 3);
-    enqueue(q, 4);
-    enqueue(q, 5);
-    display(q);
-    // inserting while the queue is full
-    enqueue(q, 1);
+    initialize(&q);
+    display(&q);
+    enqueue(&q, 1);
+    enqueue(&q, 2);
+    enqueue(&q, 3);
+    enqueue(&q, 4);
+    enqueue(&q, 5);
+    display(&q);
+    enqueue(&q, 1); // inserting while the queue is full
     // deleting for making some space
-    dequeue(q);
-    dequeue(q);
+    dequeue(&q);
+    display(&q);
+    dequeue(&q);
+    display(&q);
     // inserting after deleting and making some space
-    enqueue(q,6);
-    enqueue(q,7);
-    enqueue(q,6);
-    display(q);
-    dequeue(q);
-    dequeue(q);
-    dequeue(q);
-    dequeue(q);
+    enqueue(&q,6);
+    display(&q);
+    enqueue(&q,7);
+    display(&q);
+    enqueue(&q,6);
+    display(&q);
+    dequeue(&q);
+    dequeue(&q);
+    dequeue(&q);
+    dequeue(&q);
+    display(&q);
+    dequeue(&q);
+    display(&q);
     return 0;
 }
